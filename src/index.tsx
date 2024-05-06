52a63037f5562149722fab891ef22a2944348621
Joe Del Balzo <jdelbalzo99@gmail.com>
2024-05-06 10:57:35 -0400

    second commit

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
-const container = document.querySelector("#root")!;
+const container = document.querySelector("#joesJavaScriptExample")!;
 const root = createRoot(container);
 
 root.render(
