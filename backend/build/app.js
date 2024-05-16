80e9e5178d8c8dac95258bc61816a8262427353b
Joe Del Balzo <jdelbalzo99@gmail.com>
2024-05-16 10:49:28 -0400

    backend v.000004

--- backend/build/app.js
+++ backend/build/app.js
@@ -5,26 +5,34 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const path_1 = __importDefault(require("path"));
-// import { fileURLToPath } from "url";
 const cors_1 = __importDefault(require("cors"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const ccs_1 = __importDefault(require("./ccs"));
 const app = (0, express_1.default)();
+console.log("the fuck is happening?");
 app.use(express_1.default.json());
 app.use((0, cors_1.default)());
 app.use(body_parser_1.default.json());
 app.use(body_parser_1.default.urlencoded({ extended: false }));
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-app.use("/yhg-assessment", express_1.default.static(path_1.default.join(__dirname, "../front")));
+// app.use("/yhg-assessment", express.static(path.join(__dirname, "../frontend/dist")));
 app.use("/api/ccs", 
 // restrictAccess,
 ccs_1.default);
 app.get("*", (req, res) => {
-    if (req.params.id) {
-        console.log("rendering");
-    }
-    res.sendFile(path_1.default.join(__dirname, "../frontend/dist", "index.html"));
+    console.log(`Serving index.html for ${req.originalUrl}`);
+    const indexPath = path_1.default.join(__dirname, "../frontend/dist", "index.html");
+    const indexPath2 = path_1.default.join(__dirname, "../../frontend/dist", "index.html");
+    console.log("1", indexPath);
+    console.log("2", indexPath2);
+    res.sendFile(indexPath, function (err) {
+        if (err) {
+            console.log(err);
+            res.sendFile(indexPath2, function (err2) {
+                console.log(err2);
+                res.status(500).send("Error serving file: " + err.message);
+            });
+        }
+    });
 });
 exports.default = app;
 //# sourceMappingURL=app.js.map
--- backend/build/ccs.js
+++ backend/build/ccs.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -6,40 +15,37 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const googleapis_1 = require("googleapis");
 const express_1 = __importDefault(require("express"));
 const ccs = (0, express_1.default)();
-// import path from "path";
-// import { fileURLToPath } from "url";
-// import fs from "fs";
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
 const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
+const path_1 = __importDefault(require("path"));
+dotenv_1.default.config({ path: path_1.default.resolve(__dirname, "../.env") });
 ccs.use(express_1.default.json());
-console.log("testing!");
-const authenticateGoogleSheets = async () => {
+// console.log("testing!");
+const authenticateGoogleSheets = () => __awaiter(void 0, void 0, void 0, function* () {
     const auth = new googleapis_1.google.auth.GoogleAuth({
         keyFile: process.env.GOOGLE_APPLICATION_CREDENTIALS,
         scopes: "https://www.googleapis.com/auth/spreadsheets",
     });
-    const client = await auth.getClient();
+    const client = yield auth.getClient();
     const googleSheets = googleapis_1.google.sheets({ version: "v4", auth: client });
     return googleSheets;
-};
-ccs.get("/:id", async (req, res) => {
+});
+ccs.get("/:id", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    // console.log("let's do some testing.");
     try {
         const code = req.params.id;
-        if (typeof code !== "string" || !/^\d{4}$/.test(code)) {
-            return res.status(400).send("Invalid code format");
-        }
-        const googleSheets = await authenticateGoogleSheets();
+        console.log("submitted code");
+        // if (typeof code !== "string" || !/^\d{4}$/.test(code)) {
+        //   return res.status(400).send("Invalid code format");
+        // }
+        const googleSheets = yield authenticateGoogleSheets();
         const spreadsheetId = "1iE0mqWwUtLUPh0NOEMoM1q87Kt7OBzS-OVzSkM1gvl4";
-        const range = "CCs_Printing_1";
-        const response = await googleSheets.spreadsheets.values.get({
+        const range = "UNIQUE CODES";
+        const response = yield googleSheets.spreadsheets.values.get({
             spreadsheetId,
             range,
         });
         //finds all rows
         const rows = response.data.values;
-        console.log(rows);
         //confirms rows exist
         if (!rows) {
             return res.status(404).send("No data found in the sheet");
@@ -48,17 +54,19 @@ ccs.get("/:id", async (req, res) => {
         const codeIndex = rows.findIndex((row) => row[0] === code);
         // const codeUsed: string = rows[codeIndex][1];
         // const codeMax: string = rows[codeIndex][2];
-        const codeValid = rows[codeIndex][3];
+        const codeValid = rows[codeIndex][1];
         //if current code isn't valid, sends a 404
-        if (codeValid == "FALSE") {
+        if (codeValid == "USED") {
+            console.log("this code has been used");
             return res.status(404).send("This code is no longer valid.");
         }
         if (codeIndex === -1) {
             return res.status(404).send("Code not found");
         }
         // Increase the use count in the Google Sheet
-        rows[codeIndex][1] = parseInt(rows[codeIndex][1]) + 1;
-        await googleSheets.spreadsheets.values.update({
+        // rows[codeIndex][1] = parseInt(rows[codeIndex][1]) + 1;
+        rows[codeIndex][1] = "USED";
+        yield googleSheets.spreadsheets.values.update({
             spreadsheetId,
             range: `${range}!B${codeIndex + 1}`,
             valueInputOption: "RAW",
@@ -66,12 +74,12 @@ ccs.get("/:id", async (req, res) => {
                 values: [[rows[codeIndex][1]]],
             },
         });
-        res.status(200).send("Code found and incremented");
+        res.status(200).send("Code found!");
     }
     catch (error) {
         console.error("Error during Google Sheets API call:", error);
         res.status(500).send("Server error: " + error);
     }
-});
+}));
 exports.default = ccs;
 //# sourceMappingURL=ccs.js.map
