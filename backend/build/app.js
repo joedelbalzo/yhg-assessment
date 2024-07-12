fa54e0bec9948c42a69a443f2d03d24927628ebc
Joe Del Balzo <jdelbalzo99@gmail.com>
2024-07-12 12:23:14 -0400

    cors test

--- backend/build/app.js
+++ backend/build/app.js
@@ -5,8 +5,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const path_1 = __importDefault(require("path"));
-// import cors, { CorsOptionsDelegate, CorsRequest } from "cors";
 const cors_1 = __importDefault(require("cors"));
+// import cors from "cors";
 // import ccs from "./ccs";
 const gas_1 = __importDefault(require("./gas"));
 const recaptcha_1 = __importDefault(require("./recaptcha"));
@@ -17,41 +17,39 @@ const limiter = (0, express_rate_limit_1.default)({
     max: 100,
     message: "Too many requests from this IP, please try again after 15 minutes",
 });
-// const whitelist: string[] = [
-//   "https://yhg-code-redemption.onrender.com",
-//   "https://yhg-code-redemption.onrender.com/",
-//   "https://yourhiddengenius.com",
-//   "https://yourhiddengenius.com/",
-//   "https://www.yourhiddengenius.com",
-//   "https://www.yourhiddengenius.com",
-//   "https://www.yourhiddengenius.com/",
-//   "https://daisy-buttercup-j6mf.squarespace.com",
-//   "https://daisy-buttercup-j6mf.squarespace.com/",
-//   "http://localhost:3000",
-// ];
-// const corsOptions: CorsOptionsDelegate = (
-//   req: CorsRequest,
-//   callback: (err: Error | null, options?: cors.CorsOptions | undefined) => void
-// ) => {
-//   const request = req as Request;
-//   const origin = req.headers.origin;
-//   if (!origin) {
-//     if (request.path.startsWith("/api")) {
-//       callback(new Error("API access without origin is not allowed"), { origin: false });
-//     } else {
-//       callback(null, { origin: true });
-//     }
-//   } else if (whitelist.includes(origin)) {
-//     callback(null, { origin: true });
-//   } else {
-//     callback(new Error("Not allowed by CORS"), { origin: false });
-//   }
-// };
+const whitelist = [
+    "https://yhg-code-redemption.onrender.com",
+    "https://yhg-code-redemption.onrender.com/",
+    "https://yourhiddengenius.com",
+    "https://yourhiddengenius.com/",
+    "https://www.yourhiddengenius.com",
+    "https://www.yourhiddengenius.com",
+    "https://www.yourhiddengenius.com/",
+    "https://daisy-buttercup-j6mf.squarespace.com",
+    "https://daisy-buttercup-j6mf.squarespace.com/",
+    "http://localhost:3000",
+];
+const corsOptions = (req, callback) => {
+    const request = req;
+    const origin = req.headers.origin;
+    if (!origin) {
+        if (request.path.startsWith("/api")) {
+            callback(new Error("API access without origin is not allowed"), { origin: false });
+        }
+        else {
+            callback(null, { origin: true });
+        }
+    }
+    else if (whitelist.includes(origin)) {
+        callback(null, { origin: true });
+    }
+    else {
+        callback(new Error("Not allowed by CORS"), { origin: false });
+    }
+};
 const app = (0, express_1.default)();
 app.set("trust proxy", 1);
-app.use((0, cors_1.default)({
-    origin: "https://www.yourhiddengenius.com",
-}));
+app.use((0, cors_1.default)(corsOptions));
 app.use((0, helmet_1.default)({
     contentSecurityPolicy: false,
 }));
